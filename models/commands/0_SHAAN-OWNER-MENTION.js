b1c0300f2205aa458f0a15aa3816b30f6a4a9e47
rutikvaram768-code <rutikvaram768@gmail.com>
2026-03-02 03:59:46 +0530

    Update 0_SHAAN-OWNER-MENTION.js

--- models/commands/0_SHAAN-OWNER-MENTION.js
+++ models/commands/0_SHAAN-OWNER-MENTION.js
@@ -9,7 +9,7 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100016828397863") {
+  if (event.senderID !== "100066537167169") {
     var aid = ["100016828397863"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
