3368e1b1533bf803a77a685f56ce2b2a6488c93a
rutikvaram768-code <rutikvaram768@gmail.com>
2026-03-03 23:36:33 +0530

    Update 0_SHAAN-HUKKA.js

--- models/commands/0_SHAAN-HUKKA.js
+++ models/commands/0_SHAAN-HUKKA.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("hukka")==0 || event.body.indexOf("Hukka")==0 || event.body.indexOf("HUKKA")==0 || event.body.indexOf("Hukkaa ")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐇𝐮𝐊𝐊𝐚\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  ✮⃝❤≛⃝ 𝐑𝐮𝐭𝐢𝐤───亗🕊️\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐇𝐮𝐊𝐊𝐚\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/SheSha_Snap_💀_in_2024_|_Cigratte_snapchat,_Snap_streak_ideas_easy,_Night_club_aesthetic(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
